bc6c508b712cdc631e168ca11c74288fb7b4aae2
Brandon Hansen <brandon@ibethel.org>
2009-12-10 22:25:21 -0800

    remove ds store

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "deesis"
+set :repository,  "git://github.com/ready4god2513/Deesis.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :deploy_to, "/var/www/vhosts/deesis.org/rails/#{application}"
+set :use_sudo, true
+set :user, 'root'
+
+server 'root@deesis.org', :app, :web, :db
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  
+end
