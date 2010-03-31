9c9b860c6cc68789f3b3738211bcee81cf861547
Brandon <brandonh@ibethel.org>
2010-03-30 19:57:24 -0700

    Compass

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,66 @@
 
 ActiveRecord::Schema.define(:version => 20100325232033) do
 
+  create_table "activities", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "activity"
+    t.string   "link_to"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "activity_stream_preferences", :force => true do |t|
+    t.string   "activity"
+    t.string   "location"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activity_stream_preferences", ["activity", "user_id"], :name => "activity_stream_preferences_idx"
+
+  create_table "activity_stream_totals", :force => true do |t|
+    t.string   "activity"
+    t.integer  "object_id"
+    t.string   "object_type"
+    t.float    "total",       :limit => 25, :default => 0.0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activity_stream_totals", ["activity", "object_id", "object_type"], :name => "activity_stream_totals_idx"
+
+  create_table "activity_streams", :force => true do |t|
+    t.string   "verb"
+    t.string   "activity"
+    t.integer  "actor_id"
+    t.string   "actor_type"
+    t.string   "actor_name_method"
+    t.integer  "count",                       :default => 1
+    t.integer  "object_id"
+    t.string   "object_type"
+    t.string   "object_name_method"
+    t.integer  "indirect_object_id"
+    t.string   "indirect_object_type"
+    t.string   "indirect_object_name_method"
+    t.string   "indirect_object_phrase"
+    t.integer  "status",                      :default => 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activity_streams", ["actor_id", "actor_type"], :name => "activity_streams_by_actor"
+  add_index "activity_streams", ["indirect_object_id", "indirect_object_type"], :name => "activity_streams_by_indirect_object"
+  add_index "activity_streams", ["object_id", "object_type"], :name => "activity_streams_by_object"
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", :force => true do |t|
     t.integer  "prayer_id"
     t.integer  "user_id"
