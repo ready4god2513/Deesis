453fb54cee3f299aa827f7a0e26b9e6098a56197
brandon <brandon@bethel-churchs-imac.local>
2010-04-06 09:30:56 -0700

    Deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,22 @@
+default_run_options[:pty] = true
+default_environment['PATH'] = "/opt/ruby-enterprise-1.8.7-2010.01/bin:$PATH"
+
 set :application, "Deesis"
 set :repository,  "git://github.com/ready4god2513/Deesis.git"
+set :domain, 'deesis.org' 
 set :scm, "git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/vhosts/deesis.org/rails/deployment/#{application}"
 set :use_sudo, false
+set :scm_verbose, true
 set :user, 'deesis_ftp'
 
-server 'root@deesis.org', :app, :web, :db
+server 'deesis_ftp@deesis.org', :app, :web, :db
+
+role :app, domain 
+role :web, domain 
+role :db, domain, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
@@ -29,7 +38,7 @@ namespace :deploy do
   end
   
   
-  task :restart do
-    run "service httpd restart"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
