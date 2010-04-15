a7d017545309c35d1a7d88afdd1ff6901ad7f486
Brandon <brandon@melissa-brandon.com>
2010-04-14 22:22:57 -0700

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
 set :application, "Deesis"
 set :repository,  "http://github.com/ready4god2513/Deesis.git"
 set :domain, 'deesis.org' 
 set :scm, "git"
 set :branch, "master"
-set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
 set :deploy_to, "/var/www/vhosts/deesis.org/httpdocs"
 set :use_sudo, false
 set :user, 'deesis_ftp'
